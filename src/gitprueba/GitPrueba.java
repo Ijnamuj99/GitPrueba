e8d59d8e42652d5fbac6ba6c91d7b3eac75f9062
Diurno <Diurno@C06PC05.informatica.comercio>
2023-11-30 10:50:59 +0100

    añadido hola mundo

---
+++ src/gitprueba/GitPrueba.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitprueba;
+
+/**
+ *
+ * @author Diurno
+ */
+public class GitPrueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("estoy haciendo u cambio");
+        // https://github.com/Ijnamuj99/GitPrueba.git
+    }
+    
+}
